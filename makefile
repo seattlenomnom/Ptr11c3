642a539faabd7fa0dac46f7c043f2465ff6d8405
seattlenomnom <mark.crapser@gmail.com>
2023-05-31 21:54:26 -0700

    initial commit

---
+++ makefile
@@ -0,0 +1,4 @@
+# makefile for ptr11c3
+#
+ptr11c3 : ptr11c3.c
+	gcc -g ptr11c3.c -o ./Build/ptr11c3
---
+++ ptr11c3.c
@@ -0,0 +1,22 @@
+/* ptr11c3.c
+ * C file for ptr11c3 an example program for Kochan, Stephen, "Programming
+ * in C 3rd Ed."
+ * "contents of a pointer used in a calculation"
+ *
+*/
+
+#include <stdio.h>
+
+int main(void) {
+    int i1, i2;
+    int *p1, *p2;
+
+    i1 = 5;
+    p1 = &i1;
+    i2 = *p1 / 2 + 10;
+    p2 = p1;
+
+    printf("i1 = %i, i2 = %i, *p1 = %i, *p2 = %i\n", i1, i2, *p1, *p2);
+
+    return(0);
+}
